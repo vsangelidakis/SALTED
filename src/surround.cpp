26fef74f19f4fc67c26c376f22725477ffa92858
Vasileios Angelidakis <vasileios.angelidakis@fau.de>
2022-05-02 13:46:31 +0200

    Document attributes of surround class

--- src/surround.cpp
+++ src/surround.cpp
@@ -2,13 +2,13 @@
 void surroundtype::init_grid()
 {
 	box.erase(box.begin(),box.end());
-	box.resize(nx);
+	box.resize(nx); // Make box with size [nx*1]
 	izmax.resize(nx);
-	for(int i=0;i<nx;i++) {box[i].resize(ny);izmax[i].resize(ny);}
-	for(int i=0;i<nx;i++) 
-	for(int j=0;j<ny;j++)
+	for(int i=0; i<nx; i++) {box[i].resize(ny); izmax[i].resize(ny);} // Resize box to [nx*ny]
+	for(int i=0; i<nx; i++)
+	for(int j=0; j<ny; j++)
 	{
-		box[i][j].resize(nz);
+		box[i][j].resize(nz); // Resize box to [nx*ny*nz]
 		izmax[i][j]=0;
 	}
 }
